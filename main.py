0ed81bb98fbe6a59279f0eb66a53134d9ea21893
Lee Jae Sun <ze_s96@naver.com>
2023-05-23 17:29:12 +0900

    no message

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def add_func(n1, n2) :
 
 def sub_func(n1, n2) :
     return n1 - n2
+
+def mul_func(n1, n2) :
+    return n1 * n2
+
+def div_func(n1, n2) :
+    return n1 / n2
     
 
 ## 전역 변수부(= 인스턴스 변수)
@@ -16,3 +22,9 @@ print(num1, '+', num2, '=', res)
 
 res = sub_func(num1, num2)
 print(num1, '-', num2, '=', res)
+
+res = mul_func(num1, num2)
+print(num1, '*', num2, '=', res)
+
+res = div_func(num1, num2)
+print(num1, '/', num2, '=', res)
